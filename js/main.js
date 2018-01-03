a5abeeee1ac9af832a53cbec8ed6b8b0b881a507
Dharma Sanjyal <rajsan44@gmail.com>
2018-01-02 21:51:26 -0800

    second commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
+//Arrow function to be used when the document loads
+$(document).ready(() => {
+
+$('#buyBtn1').click('buyBoxers', (e) =>{
+  let products  = $('#productInfo');
+  buyBoxers(products);
+  e.preventDefault();
+});
+});
 function buyBoxers(){
-	var div = document.createElement('div')
-	var heading = document.createElement('h4')
-	var text = document.createTextNode("Our Products")
+	var div = document.createElement('div');
+	var heading = document.createElement('h4');
+	var text = document.createTextNode('Our Products');
 
-	var image = document.createElement('img')
-	image.setAttribute('src', 'images/products.jpg')
+	var image = document.createElement('img');
+	image.setAttribute('src', 'images/products.jpg');
 
-	div.className="container-fluid image-wrapper"
-	div.id="productInfo"
+	div.className="container-fluid image-wrapper";
+	div.id="productInfo";
 
 	heading.appendChild(text);
 	div.appendChild(heading);
 	div.appendChild(image);
 
-	document.write(div);
-	//document.getElementById('items').appendChild(div);
-    //document.getElementById('items').innerHtml = div;
-	console.log(buyBoxers)
+   //document.getElementById('items').innerHTML = div;
+	console.log(buyBoxers);
 
 }
-document.getElementById('buyBtn1').addEventListener('click', buyBoxers, false);
+//document.getElementById('buyBtn1').addEventListener('click', buyBoxers, false);
+//document.getElementById('buyBtn2').addEventListener('click', buyBoxers, false);
